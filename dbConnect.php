f248b124fc0c9cbf00c7a2e5cd92badfea572fce
TianTaljard <151172@rgu.ac.uk>
2017-03-11 12:08:01 +0000

    sd

--- dbConnect.php
+++ dbConnect.php
@@ -27,7 +27,7 @@ $sql_query = "SELECT marvelMovieID,yearReleased,title,productionStudio,notes FRO
 $result = $link->query($sql_query);
 while($row = $result->fetch_array()){
     // print out fields from row of data
-    echo "<p>\". $row ['marvelMovieID']". " - ". $row ['title']." - ".$row ['notes']."</p>";
+    echo "<p>\. $row ['marvelMovieID']". " - ". $row ['title']." - ".$row ['notes']."</p>";
 
 }
 $result->close();
