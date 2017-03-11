10547598c1de0c16cb6f0b6838ef6dfe2a55fd73
TianTaljard <151172@rgu.ac.uk>
2017-03-11 08:41:37 +0000

    sd

--- dbConnect.php
+++ dbConnect.php
@@ -14,10 +14,13 @@ foreach ($_SERVER as $key => $value) {
 }
 $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
 
+
+
 if (!$link) {
     echo "Error: Unable to  connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
 }
+else {echo "I guess connected";}
 ?>
