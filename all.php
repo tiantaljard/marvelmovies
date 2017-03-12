56357bd5735ed4e65dff69d599132c5dcfcfa5fe
TianTaljard <151172@rgu.ac.uk>
2017-03-12 13:19:40 +0000

    sd

--- all.php
+++ all.php
@@ -12,12 +12,12 @@
     <section>
         <h2>List of all Marvel Movies</h2>
         <?
-        include 'php\dbConnect.php';
+        include 'dbConnect.php';
         $sql_query = "SELECT marvelMovieID,yearReleased,title,productionStudio,notes FROM marvelmovies where UPPER(productionStudio) like '%FOX%'; ";
         $result = $link->query($sql_query);
         while($row = $result->fetch_array()){
             // print out fields from row of data
-            echo "<p>".$row ['marvelMovieID']. " - ". $row ['title']." - ".$row ['productionStudio'].$db."</p>";
+            echo "<p>".$row ['marvelMovieID']. " - ". $row ['title']." - ".$row ['productionStudio']."</p>";
 
         }
         $result->close();
