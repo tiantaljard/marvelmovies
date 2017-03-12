7659b2d98f8a30017e4ffc267764a677d2cf07b9
TianTaljard <151172@rgu.ac.uk>
2017-03-12 11:37:33 +0000

    sd

--- index.php
+++ index.php
@@ -13,11 +13,11 @@
         <h2>Connect to  database</h2>
         <?
         include 'utils\dbConnect.php';
-        print "<p> $db </p>";
+        print "<p> hello </p>";
         ?>
         <p><a href="all.php">All Marvel Movies</a></p>
         <p><a href="xmen.php">All X-MEN Movies</a></p>
-        <p><a href="utils\dbConnect.php">dbC7onnect</a></p>
+        <p><a href="utils\dbConnect.php">dbC8onnect</a></p>
 
     </section>
 
