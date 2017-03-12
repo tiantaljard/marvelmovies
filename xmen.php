43280e9dc9cf5cf186dbc2cd7794a4428b583c83
TianTaljard <151172@rgu.ac.uk>
2017-03-12 08:40:52 +0000

    sd

--- xmen.php
+++ xmen.php
@@ -13,15 +13,16 @@
         <h2>List of all Marvel Movies</h2>
         <?
         include 'dbConnect.php';
-        $sql_query = "SELECT marvelMovieID,yearReleased,title,productionStudio,notes,CHAR_LENGTH (notes) as llen FROM marvelmovies where UPPER(title) like '%MEN%'; ";
+        $sql_query = "SELECT marvelMovieID,yearReleased,title,productionStudio,notes,CHAR_LENGTH (notes) as llen, is_null(notes) as isnull FROM marvelmovies where UPPER(title) like '%MEN%'; ";
         $result = $link->query($sql_query);
         while($row = $result->fetch_array()){
             // print out fields from row of data
             echo "<p>".$row ['marvelMovieID']. " - ". $row ['yearReleased']." - ".$row ['title']." - ".$row ['productionStudio']."</p>";
             $notes=$row ['notes'];
             $len=$row ['llen'];
+            $isnull=$row ['isnull'];
             if ($len>0) {
-                echo "<p>$notes, $len,TT</p>";
+                echo "<p>$notes, $len,TT,isnull</p>";
             }
 
 
