80b7a943a40fb37854d9534a3c384d60b48e69b8
TianTaljard <151172@rgu.ac.uk>
2017-03-12 07:20:29 +0000

    sd

---
+++ all.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>All Marvel Movies</title>
+</head>
+<body>
+<header>
+    <h1>All Marvel Movies</h1>
+</header>
+<main>
+    <section>
+        <h2>List of all Marvel Movies</h2>
+        <?
+        $sql_query = "SELECT marvelMovieID,yearReleased,title,productionStudio,notes FROM marvelmovies; ";
+        $result = $link->query($sql_query);
+        while($row = $result->fetch_array()){
+            // print out fields from row of data
+            echo "<p>".$row ['marvelMovieID']. " - ". $row ['title']." - ".$row ['notes']."</p>";
+
+        }
+        $result->close();
+        $link->close();
+        ?>
+
+
+
+    </section>
+
+</main>
+<footer>
+</footer>
+</body>
+</html>
