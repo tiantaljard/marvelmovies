28459c07f6d9bd65f82918956a895ce6e20ac7a1
TianTaljard <151172@rgu.ac.uk>
2017-03-12 10:43:00 +0000

    sd

--- index.php
+++ index.php
@@ -16,8 +16,9 @@
         ?>
         <p><a href="all.php">All Marvel Movies</a></p>
         <p><a href="xmen.php">All X-MEN Movies</a></p>
-        <p>$db</p>
-
+        <?php
+        echo "<p>".$row ['$db']. " - "."</p>";
+        ?>
     </section>
 
 </main>
