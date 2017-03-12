00189f9476e4d685146b272b4c4e3ffb734925ae
TianTaljard <151172@rgu.ac.uk>
2017-03-12 11:15:43 +0000

    sd

--- utils/dbConnect.php
+++ utils/dbConnect.php
@@ -20,7 +20,8 @@ define('DB_USERNAME', $connectstr_dbname);
 define('DB_PASSWORD', $connectstr_dbusername);
 define('DB_DATABASE', $connectstr_dbpassword);
 $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-
+echo $db;
+echo $link;
 
 
 if (!$link) {
