18de007cf2e9be3c6e8638f9fb7837a769f411d1
TianTaljard <151172@rgu.ac.uk>
2017-03-12 11:25:18 +0000

    sd

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
         ?>
         <p><a href="all.php">All Marvel Movies</a></p>
         <p><a href="xmen.php">All X-MEN Movies</a></p>
-        <p><a href="utils\dbConnect.php">dbC3onnect</a></p>
-        <p>$db</p>
+        <p><a href="utils\dbConnect.php">dbC4onnect</a></p>
+        <p>{$db}</p>
 
     </section>
 
