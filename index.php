579fbd2ee3f146fa63f8f57406664b71efae4662
TianTaljard <151172@rgu.ac.uk>
2017-03-12 07:15:45 +0000

    sd

--- index.php
+++ index.php
@@ -12,43 +12,12 @@
     <section>
         <h2>Connect to  database</h2>
         <?
-        include dbConnect.php;
+        include 'dbConnect.php';
         ?>
         <p><a href="dbConnect.php">Connect</a></p>
 
         <?php
-        $connectstr_dbhost = '';
-        $connectstr_dbname = '';
-        $connectstr_dbusername = '';
-        $connectstr_dbpassword = '';
-        foreach ($_SERVER as $key => $value) {
-            if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-                continue;
-            }
-            $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-            $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-            $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-            $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-        }
-        $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword, $connectstr_dbname);
-
-
-
-        if (!$link) {
-            echo "Error: Unable to  connect to MySQL." . PHP_EOL;
-            echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-            echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-            exit;
-        }
-
-
-
-        $sql_query = "SELECT marvelMovieID,yearReleased,title,productionStudio,notes FROM marvelmovies; ";
-        $result = $link->query($sql_query);
-        while($row = $result->fetch_array()) {
-            // print out fields from row of data
-            echo "<p>" . $row ['marvelMovieID'] . " - " . $row ['title'] . " - " . $row ['notes'] . "</p>";
-        }
+        ;
         ?>
 
     </section>
