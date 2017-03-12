654a4bab0751ce93f76cf1b48dc2c149ed962e36
TianTaljard <151172@rgu.ac.uk>
2017-03-12 11:21:07 +0000

    sd

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
         ?>
         <p><a href="all.php">All Marvel Movies</a></p>
         <p><a href="xmen.php">All X-MEN Movies</a></p>
-        <p><a href="utils\dbConnect.php">dbC onnect</a></p>
+        <p><a href="utils\dbConnect.php">dbC2onnect</a></p>
 
     </section>
 
--- utils/dbConnect.php
+++ utils/dbConnect.php
@@ -27,7 +27,7 @@ echo $link;
 
 
 if (!$link) {
-    echo "Error: Unable to  connect to MySQL." . PHP_EOL;
+    echo "Error: Una ble to  connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
     exit;
